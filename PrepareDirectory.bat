ebe674e524aed8ed86008ff6fd75e1aeebb399ff
Kyle A. Burrows <KBurrows@skylinesoft.com>
2018-06-04 09:53:16 -0400

    Fixed error where folders would not get created

--- PrepareDirectory.bat
+++ PrepareDirectory.bat
@@ -3,30 +3,29 @@
 echo Preparing Path: %cd%...
 
 echo Preparing CA directory...
-mkdir DoD/CA/certs
-mkdir DoD/CA/crl
-mkdir DoD/CA/newcerts
-mkdir DoD/CA/private
-mkdir DoD/CA/signing
-echo nul > DoD/CA/index.txt
-echo 1000 > DoD/CA/serial
+mkdir DoD\CA\certs
+mkdir DoD\CA\crl
+mkdir DoD\CA\newcerts
+mkdir DoD\CA\private
+type nul > DoD\CA\index.txt
+echo 1000 > DoD\CA\serial
 
 echo Preparing Intermediate directory...
-echo nul > DoD/CA/Intermediate/index.txt
-echo 1000 > DoD/CA/Intermediate/serial
-echo 1000 > DoD/CA/Intermediate/crlnumber
-mkdir DoD/CA/Intermediate/certs
-mkdir DoD/CA/Intermediate/crl
-mkdir DoD/CA/Intermediate/csr
-mkdir DoD/CA/Intermediate/newcerts
-mkdir DoD/CA/Intermediate/private
+type nul > DoD\CA\Intermediate\index.txt
+echo 1000 > DoD\CA\Intermediate\serial
+echo 1000 > DoD\CA\Intermediate\crlnumber
+mkdir DoD\CA\Intermediate\certs
+mkdir DoD\CA\Intermediate\crl
+mkdir DoD\CA\Intermediate\csr
+mkdir DoD\CA\Intermediate\newcerts
+mkdir DoD\CA\Intermediate\private
 
 echo Preparing Signing directory...
-echo nul > DoD/CA/Signing/index.txt
-echo 1000 > DoD/CA/Signing/serial
-echo 1000 > DoD/CA/Signing/crlnumber
-mkdir DoD/CA/Signing/certs
-mkdir DoD/CA/Signing/crl
-mkdir DoD/CA/Signing/csr
-mkdir DoD/CA/Signing/newcerts
-mkdir DoD/CA/Signing/private
+type nul > DoD\CA\Signing\index.txt
+echo 1000 > DoD\CA\Signing\serial
+echo 1000 > DoD\CA\Signing\crlnumber
+mkdir DoD\CA\Signing\certs
+mkdir DoD\CA\Signing\crl
+mkdir DoD\CA\Signing\csr
+mkdir DoD\CA\Signing\newcerts
+mkdir DoD\CA\Signing\private
